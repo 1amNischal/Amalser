f3d969816f5bc340213e74f250aeca2d1da57b3f
1amNischal <87352881+1amNischal@users.noreply.github.com>
2021-11-30 20:21:52 +0530

    Update xlove.js

--- plugins/xlove.js
+++ plugins/xlove.js
@@ -5,25 +5,23 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: "Source Code Will Be On Github Soon!"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/w89FHm7.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.ibb.co/FbMnwYF/logo.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name: Amalser*
 
-*Creator number : wa.me/919895828468?text=Hi%20Amal%20Ser.%20*
+*Creator number : wa.me/918919822103?text=Hi%20Amal%20Ser.%20*
 
  *To check update .update   To update Bot .update now*
 
- *Bot making video : https://youtu.be/Tb1B-rS52uo*
+ Bot Made With ❤️ By Nischal*
      
- *Instagram id: https://www.instagram.com/p/CCdcH3FBd1a/?utm_medium=copy_link*
+ *Instagram id: https://www.instagram.com/i.am.nischal/?utm_medium=copy_link*
 
- *Yt_PASSWORD: _________(password on this video watch it full)*
+ *githublink : Will Be Available Soon*
 
- *githublink : https://github.com/Amal-ser/Amalser*
-
- *Amalser New Acoount Git Link Old Account Is Flagged Hope you Enjoy This Thanku 🥰*
+ Nishi-Bot Made with ❤️ By Nischal🥰*
 `}) 
 
 }));
